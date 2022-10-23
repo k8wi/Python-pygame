86f525695a833038d4f501a19211ed87030852bd
Thushar R <thushar.r02@gmail.com>
2022-10-23 16:08:41 +0530

    spaceship Movement Mechanism complete

--- main.py
+++ main.py
@@ -36,14 +36,23 @@ while running:
         if event.key==pygame.K_RIGHT:
             print("RIGHT arrow pressed")
             Xchange=0.3
+        if event.key==pygame.K_UP:
+            print("UP arrow pressed")
+            Ychange=-0.3
+        if event.key==pygame.K_DOWN:
+            print("DOWN arrow pressed")
+            Ychange=0.3
+
     if event.type == pygame.KEYUP:
-        if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+        if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT or event.key==pygame.K_UP or event.key==pygame.K_DOWN:
             print("Keystroke released")
             Xchange=0
+            Ychange=0
 
 
-    screen.fill((128,128,128))
+    screen.fill((20,20,20))
     spaceshipX+=Xchange
+    spaceshipY+=Ychange
     spaceship(spaceshipX,spaceshipY)
     pygame.display.update()
 
