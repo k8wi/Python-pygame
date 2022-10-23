16ae4745259c27af098f163f1101d1e51357f690
Thushar R <thushar.r02@gmail.com>
2022-10-23 15:38:19 +0530

    spaceship, icon, background and title added

--- main.py
+++ main.py
@@ -7,9 +7,27 @@ pygame.init()
 # screen creation
 screen = pygame.display.set_mode((800, 600))
 
+#screen title & icon
+pygame.display.set_caption("Space Game")
+icon=pygame.image.load('images/gameicon.png')
+pygame.display.set_icon(icon)
+
+#Spaceship
+spaceshipIMG=pygame.image.load('images/spaceship.png')
+spaceshipX=370
+spaceshipY=480
+
+def spaceship():
+  screen.blit(spaceshipIMG,(spaceshipX,spaceshipY))
+
 # infinite loop for game
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((128,128,128))
+    spaceship()
+    pygame.display.update()
+
